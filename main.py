132dcb906de9d7234855ab6d8ee2be910d5d3b1a
BartuKaptanoglu <bartu.kaptanoglu@gmail.com>
2024-08-08 13:21:55 +0300

    initial commit

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import supervision as sv
 from inference.models.utils import get_roboflow_model
 import gradio as gr
 from PIL import Image
-
+import requests
 model = get_roboflow_model(model_id="object-xos6g/2", api_key="#####")
-
+from sahi.utils.file import download_from_url
+url="https://raw.githubusercontent.com/BartuKaptanoglu/gradio-bicycle/main/bisiklet.jpg",
+download_from_url(
+    "https://raw.githubusercontent.com/BartuKaptanoglu/gradio-bicycle/main/bisiklet.jpg",
+    "bisiklet1.jpg"
+)
 def func(image_path):
     image = cv2.imread(image_path)
     results = model.infer(image)[0]
@@ -22,7 +27,9 @@ def func(image_path):
     im_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
     im = Image.fromarray(im_rgb)
     return gr.Plot(im)
+example=[
+    ["bisiklet1.jpg"]
+]
 
-
-demo = gr.Interface(func, gr.Image(type="filepath"), outputs="image",title="Yolo bike")
+demo = gr.Interface(func, gr.Image(type="filepath"), outputs="image",title="Yolo bike",examples=example)
 demo.launch(debug=True)
